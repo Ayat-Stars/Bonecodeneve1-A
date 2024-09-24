c8cb7fc0652e13db380c6273fb7e64de4c36e41d
Ayatt Miky <ana.majolo@escola.pr.gov.br>
2024-09-24 10:31:08 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("rgb(55,55,119)");
+  fill("white")
+  ellipse(200, 300, 150, 150);
+  ellipse(200, 200, 120,120);
+  ellipse(200, 120, 100, 100);
+  
+  fill("rgb(176,176,240)")
+  circle(180, 110, 30, 30);
+  circle(220, 110, 30, 30);
+  fill ("white")
+  stroke("white")
+  
+  circle(190, 105, 10, 10)
+  circle(230, 105, 10, 10)
+  fill("orange")
+  stroke("black")
+  triangle(200, 130, 200, 140, 250, 135)
+  
+fill (3, 3, 3)
+  ellipse(200, 180, 9, 10)
+  ellipse(200, 200, 9, 10)
+  ellipse(200, 220, 9, 10)
+  
+  strokeWeight(4)
+  stroke(191, 48, 48)
+  line(249, 196, 318, 170)
+  line(152, 195, 65, 168)
+}
